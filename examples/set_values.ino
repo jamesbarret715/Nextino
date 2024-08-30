3a4e4dd0db2413f992c8ba829a4224d60fa826bf
James Barret <jamesbarret715@gmail.com>
2024-08-30 10:47:48 +0200

    ensure Arduino library compatibility compliance

---
+++ examples/set_values.ino
@@ -0,0 +1,26 @@
+#include <Nextino.h>
+#include <SoftwareSerial.h>
+
+// Plug the Nextion display into 5V, GND and pins 5 (TX) and 6 (RX).
+SoftwareSerial displaySerial(5, 6);
+
+Nextino::Display display(displaySerial);
+
+// In our HMI file, we have a Text element with objname "t1", and a Number with objname "n0".
+Nextino::Text heading(display, "t1");
+Nextino::Number timeElapsed(display, "n0");
+
+void setup()
+{
+    // Start the serial port at the display's baud.
+    displaySerial.begin(9600);
+
+    // Change the text in the header.
+    heading.setText("Hello, Arduino!");
+}
+
+void loop()
+{
+    // Continuously update the time elapsed (seconds).
+    timeElapsed.setValue(millis() / 1000);
+}
